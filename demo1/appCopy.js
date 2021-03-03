92a05e22345890662d7e38132fee76e690254e3c
ciciliu <ciciliu@lagou.com>
2021-03-03 19:09:06 +0800

    init project

---
+++ demo1/appCopy.js
@@ -0,0 +1,20 @@
+// 引入模块
+const CreateError = require('http-errors')
+const express = require('express')
+const path = require('path')
+const cookieParser = require('cookie-parser')
+const logger = require('morgan')
+// 引入路由
+const  indexRouter = require('./routes/index')
+const  userRouter = require('./routes/users')
+
+//实例化 express
+
+const app = express();
+
+//定义页面目录
+app.set('views',path.join(__dirname,views))
+//定义mobanyinq
+app.set('view engine','jade')
+
+//定义日志打印级别
